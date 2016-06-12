f8b352751790338f27db56c023acab072ee9fcb8
Panos Sakkos <panos.sakkos@protonmail.com>
2016-06-12 20:54:45 +0200

    v1.2.1
    - Fixes #7

--- app/src/main/java/com/dono/psakkos/dono/Dono.java
+++ app/src/main/java/com/dono/psakkos/dono/Dono.java
@@ -1,11 +1,24 @@
+// Dono Android - Password Derivation Tool
+// Copyright (C) 2016  Panos Sakkos
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package com.dono.psakkos.dono;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
 
-/**
- * Created by ghost on 4/12/16.
- */
 public class Dono
 {
     public static final int MIN_KEY_LENGTH = 17;
--- app/src/main/java/com/dono/psakkos/dono/EmptyFragment.java
+++ app/src/main/java/com/dono/psakkos/dono/EmptyFragment.java
@@ -1,3 +1,19 @@
+// Dono Android - Password Derivation Tool
+// Copyright (C) 2016  Panos Sakkos
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package com.dono.psakkos.dono;
 
 import android.app.Fragment;
@@ -7,9 +23,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by ghost on 4/11/16.
- */
 public class EmptyFragment extends Fragment
 {
     @Nullable
